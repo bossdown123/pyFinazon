964d21c4b85f38ff75befce890ecab4d280ad9b3
bossdown123 <48166550+bossdown123@users.noreply.github.com>
2023-08-17 22:07:26 +0100

    Update test_core.py

--- tests/test_core.py
+++ tests/test_core.py
@@ -4,7 +4,28 @@ from finazon import Finazon
 API_KEY = 'YOUR_API_KEY'  # You can also set this up using environment variables or mocking for security.
 
 class TestFinazon(unittest.TestCase):
-
+    def test_time_series_df(self):
+        # Provide sample data for the test
+        sample_data = [
+            {
+                't': 1629888000,  # example timestamp
+                'open': 150.23,
+                'high': 153.44,
+                'low': 149.55,
+                'close': 153.12,
+                'volume': 7010200
+            },
+            {
+                't': 1629974400,  # example timestamp
+                'open': 153.00,
+                'high': 155.00,
+                'low': 152.00,
+                'close': 154.89,
+                'volume': 6910200
+            }
+        ]
+    df = self.api._time_series_df([sample_data, sample_data], ['AAPL', 'TSLA'])
+    self.assertTrue(hasattr(df, 'shape'))
     def setUp(self):
         self.api = Finazon(api_key=API_KEY)
 

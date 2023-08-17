c6872f2a410c98b1c9ba329e6e9e37fe669378c0
bossdown123 <48166550+bossdown123@users.noreply.github.com>
2023-08-17 22:01:03 +0100

    Update test_core.py

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,2 +1,27 @@
-import finazon
-from finazon.core import Finazon
+import unittest
+from finazon import Finazon
+
+API_KEY = 'YOUR_API_KEY'  # You can also set this up using environment variables or mocking for security.
+
+class TestFinazon(unittest.TestCase):
+
+    def setUp(self):
+        self.api = Finazon(api_key=API_KEY)
+
+    def test_init(self):
+        self.assertIsInstance(self.api, Finazon)
+
+    def test_send_request(self):
+        # This is a basic test to see if the function is callable.
+        # You can expand on this with more specific tests.
+        self.assertIsNotNone(self.api._send_request("/some_endpoint"))
+
+    def test_time_series_df(self):
+        # Assuming the method returns a DataFrame
+        df = self.api._time_series_df([], [])
+        self.assertTrue(hasattr(df, 'shape'))
+
+    # Add more tests for other methods similarly
+
+if __name__ == '__main__':
+    unittest.main()
